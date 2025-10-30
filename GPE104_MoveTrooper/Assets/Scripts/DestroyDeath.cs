b180ad2c7fcccca5eb7b8eab9df9a483c39ddd30
AndrewMicksch <amicksch51064@uat.edu>
2025-10-30 15:20:33 -0700

    Sounds added

--- GPE104_MoveTrooper/Assets/Scripts/DestroyDeath.cs
+++ GPE104_MoveTrooper/Assets/Scripts/DestroyDeath.cs
@@ -5,12 +5,16 @@ public class DestroyDeath : Death
 {
 
     public bool Point;
+    public AudioClip deathSFX;
     public override void Die()
     {
+        AudioSource.PlayClipAtPoint(GameManager.core.deathSFX, transform.position, 1.0f);
         Destroy(gameObject);
         if (Point == true)
         {
             GameManager.core.Score += 1;
         }
+        
+
     }
 }
