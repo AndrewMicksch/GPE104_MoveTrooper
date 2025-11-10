aa8903e25082c33ae58dd4eae914811519682be2
AndrewMicksch <amicksch51064@uat.edu>
2025-11-10 03:24:12 -0700

    Actually working

--- GPE104_MoveTrooper/Assets/Scripts/DestroyDeath.cs
+++ GPE104_MoveTrooper/Assets/Scripts/DestroyDeath.cs
@@ -5,6 +5,7 @@ public class DestroyDeath : Death
 {
 
     public bool point;
+    public bool isHIM;
     public float pointAmount;
     private AudioClip deathSFX;
 
@@ -19,6 +20,10 @@ public class DestroyDeath : Death
         if (point == true)
         {
             GameManager.core.Score += (int)(1 * pointAmount);
+        }
+        if (isHIM == true)
+        {
+            GameManager.core.winCondition = true;
         }
     }
 }
